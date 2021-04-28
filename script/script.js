ad66ed48f5fcb61983276cb3abc24495d6f007cc
DimerDev <erasovwork@gmail.com>
2021-04-28 15:23:05 +0300

    Create header, slider

---
+++ script/script.js
@@ -0,0 +1,43 @@
+function getElem(e) {
+
+   if (e.target.classList.contains('arrow')) return changePeriod(e);
+   else return false;
+
+
+}
+
+
+function changePeriod(elem) {
+   let period = document.querySelectorAll('.nav-link');
+   let count = 0;
+
+   if (elem.target.classList.contains('fa-chevron-right')) { //move forward
+
+      for (let i = 0; i < period.length; i++) {
+         if (period[i].classList.contains('active')) {
+            period[i].classList.remove('active');
+            count = i;
+         }
+      }
+      count = ++count;
+      if (count == period.length) count = 0;
+      period[count].classList.add('active');
+   }
+
+   if (elem.target.classList.contains('fa-chevron-left')) { //move back
+
+      for (let i = period.length - 1; i >= 0; i--) {
+         if (period[i].classList.contains('active')) {
+            period[i].classList.remove('active');
+            count = i;
+         }
+      }
+      count = --count;
+      if (count == -1) count = period.length - 1;
+      period[count].classList.add('active');
+   }
+
+}
+
+
+document.querySelector('.bodyTask').addEventListener('click', getElem);
